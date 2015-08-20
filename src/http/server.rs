c7a77d0f037086b202474aae53539597f0df64c1
Juan Ibiapina <juanibiapina@gmail.com>
2015-08-20 18:02:41 -0300

    refactor: Add http server abstraction

--- src/http/server.rs
+++ src/http/server.rs
@@ -3,19 +3,34 @@ extern crate hyper;
 use std::env;
 use std::thread;
 
-use self::hyper::server::Server;
+use self::hyper::server;
 
 use http::dispatcher::Dispatcher;
 use http::app::App;
 
-pub fn run() -> thread::JoinHandle<()> {
-    thread::spawn(move || {
-        let app_home = env::var("ZAS_HOME").unwrap();
-
-        let get_app = App::new("get", "12050", app_home);
-
-        let dispatcher = Dispatcher::new(get_app);
-
-        Server::http("127.0.0.1:12044").unwrap().handle(dispatcher).unwrap();
-    })
+pub struct Server {
+    pub thread: thread::JoinHandle<()>,
 }
+
+impl Server {
+    pub fn create() -> Server {
+        let thread = Server::create_thread();
+
+        Server{
+            thread: thread,
+        }
+    }
+
+    fn create_thread() -> thread::JoinHandle<()> {
+        thread::spawn(move || {
+            let app_home = env::var("ZAS_HOME").unwrap();
+
+            let get_app = App::new("get", "12050", app_home);
+
+            let dispatcher = Dispatcher::new(get_app);
+
+            server::Server::http("127.0.0.1:12044").unwrap().handle(dispatcher).unwrap();
+        })
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate zas;
 #[cfg(not(test))]
 fn main() {
     let dns_server = zas::dns::server::Server::create();
-    let http_server_thread = zas::http::server::run();
+    let http_server = zas::http::server::Server::create();
 
     dns_server.thread.join().unwrap();
-    http_server_thread.join().unwrap();
+    http_server.thread.join().unwrap();
 }
