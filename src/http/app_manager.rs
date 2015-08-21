6709a0cd266056429769b3a49e73fa00c6dfc654
Juan Ibiapina <juanibiapina@gmail.com>
2015-08-21 20:34:22 -0300

    refactor: Avoid reading app_home on each request

--- src/http/app_manager.rs
+++ src/http/app_manager.rs
@@ -9,23 +9,25 @@ const DEFAULT_APP_HOME: &'static str = "~/.zas";
 
 pub struct AppManager {
     pub next_port: u16,
+    app_home: String,
     pub apps: HashMap<String, App>,
 }
 
 impl AppManager {
     pub fn new() -> AppManager {
+        let app_home = env::var("ZAS_HOME").unwrap_or(DEFAULT_APP_HOME.to_string());
+
         AppManager {
             next_port: 12050,
+            app_home: app_home,
             apps: HashMap::new(),
         }
     }
 
     pub fn ensure_app_running(&mut self, app_name: &String) -> u16 {
-        let app_home = env::var("ZAS_HOME").unwrap_or(DEFAULT_APP_HOME.to_string());
-
         if !self.apps.contains_key(app_name) {
             let next_port = self.next_port;
-            self.apps.insert(app_name.to_string(), App::new(app_name.to_string(), next_port, &app_home));
+            self.apps.insert(app_name.to_string(), App::new(app_name.to_string(), next_port, &self.app_home));
             self.next_port = next_port + 1;
 
             block_until_port_open(next_port);
