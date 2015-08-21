181030a246c728e82acd8303176cbad2e8e531bc
Juan Ibiapina <juanibiapina@gmail.com>
2015-08-21 20:22:20 -0300

    refactor: Move app handling logic to app manager

--- src/http/app_manager.rs
+++ src/http/app_manager.rs
@@ -1,6 +1,11 @@
+use std::env;
+use std::thread;
 use std::collections::HashMap;
+use std::process::Command;
 
 use http::app::App;
+
+const DEFAULT_APP_HOME: &'static str = "~/.zas";
 
 pub struct AppManager {
     pub next_port: u16,
@@ -13,5 +18,27 @@ impl AppManager {
             next_port: 12050,
             apps: HashMap::new(),
         }
+    }
+
+    pub fn ensure_app_running(&mut self, app_name: &String) -> u16 {
+        let app_home = env::var("ZAS_HOME").unwrap_or(DEFAULT_APP_HOME.to_string());
+
+        if !self.apps.contains_key(app_name) {
+            let next_port = self.next_port;
+            self.apps.insert(app_name.to_string(), App::new(app_name.to_string(), next_port, &app_home));
+            self.next_port = next_port + 1;
+
+            block_until_port_open(next_port);
+        }
+
+        let app = self.apps.get(app_name).unwrap();
+
+        app.port
+    }
+}
+
+fn block_until_port_open(port: u16) {
+    while !Command::new("nc").arg("-z").arg("localhost").arg(format!("{}", port)).status().unwrap().success() {
+        thread::sleep_ms(300);
     }
 }
--- src/http/dispatcher.rs
+++ src/http/dispatcher.rs
@@ -1,10 +1,7 @@
 extern crate hyper;
 
-use std::env;
-use std::thread;
 use std::io::Read;
 use std::sync::Mutex;
-use std::process::Command;
 
 use self::hyper::net::Fresh;
 use self::hyper::client::Client;
@@ -14,10 +11,7 @@ use self::hyper::server::Handler;
 use self::hyper::server::Request;
 use self::hyper::server::Response;
 
-use http::app::App;
 use http::app_manager::AppManager;
-
-const DEFAULT_APP_HOME: &'static str = "~/.zas";
 
 pub struct Dispatcher {
     pub app_manager: Mutex<AppManager>,
@@ -33,27 +27,16 @@ impl Dispatcher {
 
 impl Handler for Dispatcher {
     fn handle(&self, request: Request, response: Response<Fresh>) {
-        let app_home = env::var("ZAS_HOME").unwrap_or(DEFAULT_APP_HOME.to_string());
         let host: String = request.headers.get::<Host>().unwrap().hostname.to_string();
         let app_name = host.split(".").collect::<Vec<_>>().first().unwrap().to_string();
 
-        let port: String;
+        let port: u16;
         {
             let mut app_manager = self.app_manager.lock().unwrap();
-
-            if !app_manager.apps.contains_key(&app_name) {
-                let next_port = app_manager.next_port;
-                app_manager.apps.insert(app_name.to_string(), App::new(app_name.to_string(), next_port, &app_home));
-                app_manager.next_port = next_port + 1;
-
-                block_until_port_open(next_port);
-            }
-
-            let app = app_manager.apps.get(&app_name).unwrap();
-            port = app.port.to_string();
+            port = app_manager.ensure_app_running(&app_name);
         }
 
-        let url = format!("http://localhost:{}", &port);
+        let url = format!("http://localhost:{}", port);
 
         let client = Client::new();
 
@@ -65,11 +48,5 @@ impl Handler for Dispatcher {
         app_response.read_to_string(&mut body).unwrap();
 
         response.send(&body.into_bytes()).unwrap();
-    }
-}
-
-fn block_until_port_open(port: u16) {
-    while !Command::new("nc").arg("-z").arg("localhost").arg(format!("{}", port)).status().unwrap().success() {
-        thread::sleep_ms(300);
     }
 }
