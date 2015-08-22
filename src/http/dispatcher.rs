14075e1a29c4f54527f3dcbe5877f726d98de75c
Juan Ibiapina <juanibiapina@gmail.com>
2015-08-21 23:10:04 -0300

    refactor: Extract Host header parsing logic to a method

--- src/http/dispatcher.rs
+++ src/http/dispatcher.rs
@@ -23,18 +23,20 @@ impl Dispatcher {
             app_manager: Mutex::new(app_manager),
         }
     }
+
+    fn ensure_app_running(&self, request: &Request) -> u16 {
+        let host = request.headers.get::<Host>().unwrap().hostname.to_string();
+        let app_name = host.split(".").collect::<Vec<_>>().first().unwrap().to_string();
+
+        let mut app_manager = self.app_manager.lock().unwrap();
+
+        app_manager.ensure_app_running(&app_name)
+    }
 }
 
 impl Handler for Dispatcher {
     fn handle(&self, request: Request, response: Response<Fresh>) {
-        let host: String = request.headers.get::<Host>().unwrap().hostname.to_string();
-        let app_name = host.split(".").collect::<Vec<_>>().first().unwrap().to_string();
-
-        let port: u16;
-        {
-            let mut app_manager = self.app_manager.lock().unwrap();
-            port = app_manager.ensure_app_running(&app_name);
-        }
+        let port = self.ensure_app_running(&request);
 
         let url = format!("http://localhost:{}", port);
 
