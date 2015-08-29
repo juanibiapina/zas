cc9cc76b41c6b42288562881e77e8f232316951a
Juan Ibiapina <juanibiapina@gmail.com>
2015-08-29 17:15:39 -0300

    feature: Make install scripts more verbose

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,22 +16,25 @@ mkdir -p "$ZAS_ROOT"
 cd "$ZAS_ROOT"
 
 echo "Downloading Zas version $VERSION"
-curl -sL "${ARCHIVE_URL}" | tar xzf -
+curl -L "${ARCHIVE_URL}" | tar xzf -
 
 ln -sf "zas-v${VERSION}+${PLATFORM}" current
 
+echo "Installing daemon"
 m4 --define ZAS_BINARY="$ZAS_ROOT/current/bin/zas" "$ZAS_ROOT/current/resources/com.zas.zasd.plist.template" > "${ZASD_PLIST_PATH}"
 
 launchctl bootstrap gui/"$UID" "${ZASD_PLIST_PATH}" 2>/dev/null
 launchctl enable gui/"$UID"/com.zas.zasd 2>/dev/null
 launchctl kickstart -k gui/"$UID"/com.zas.zasd 2>/dev/null
 
+echo "Installing DNS resolver"
 sudo cp -f current/resources/dev-resolver /etc/resolver/dev
 
-# set up port forwarding
-
+echo "Installing port forward"
 sudo cp current/resources/com.zas.firewall.plist "${FIREWALL_PLIST_PATH}"
 
 sudo launchctl bootstrap system "${FIREWALL_PLIST_PATH}" 2>/dev/null
 sudo launchctl enable system/com.zas.firewall 2>/dev/null
 sudo launchctl kickstart -k system/com.zas.firewall 2>/dev/null
+
+echo "Done"
